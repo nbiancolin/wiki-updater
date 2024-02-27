4eb744d1d587e601e4923b427038f5d3270d6de1
nbiancolin <117390343+nbiancolin@users.noreply.github.com>
2024-02-27 10:49:32 -0500

    put mroe fields on post request but still no luck

--- updateWiki.py
+++ updateWiki.py
@@ -5,28 +5,43 @@ import passwords
 
 def updatePageContent(new_content, teamNum = "100", username = passwords.uname, password = passwords.dokupword, pageName = "testing", url = "https://ug251.eecg.utoronto.ca/wiki297s"):
     today = date.today()
-    edit_url = f"{url}/doku.php?id=cd{teamNum}:{pageName}&do="
+    edit_url = f"{url}/doku.php?id=cd{teamNum}:{pageName}&do=edit"
     try:
         # Retrieve the edit token
         print("Attempting to send content to url: " + edit_url)
-        response = requests.get(edit_url + "edit", auth=HTTPBasicAuth(username, password))
+        response = requests.get(edit_url, auth=HTTPBasicAuth(username, password))
         if response.status_code == 200:
             edit_token = response.text.split('name="sectok" value="', 1)[1].split('"', 1)[0]
+            rev = response.text.split('name="rev" value="', 1)[1].split('"', 1)[0]
+            dokuDate = response.text.split('name="date" value="', 1)[1].split('"', 1)[0]
+            prefix = response.text.split('name="prefix" value="', 1)[1].split('"', 1)[0]
+            suffix = response.text.split('name="suffix" value="', 1)[1].split('"', 1)[0]
+            changecheck = response.text.split('name="changecheck" value="', 1)[1].split('"',1)[0]
+            #print(changecheck)
             print("This is what the updated page will look like: ")
             print(new_content)
             # Prepare data for the POST request
             #print(edit_url)
             data = {
                 #"do": "save",
+                #"date":"1709006747",
                 "sectok": edit_token,
-                "rev": "",
                 "id": f"cd{teamNum}:{pageName}",
+                "rev": rev,
+                "date": dokuDate,
+                "prefix": prefix,
+                "suffix": suffix,
+                "changecheck": changecheck,
+                "target": "section",
                 "wikitext": new_content,
                 "do[save]": "1",
-                "summary": "Updated via Nick's Wiki-Updater @ " + today.strftime('%y/%m/%d/%H/%M')
+                #"do[preview]": "0",
+                "summary": "Updated via Nick's Wiki-Updater"
             }
+            for key in data:
+                print(key + " " + data[key])
             # Send POST request to update the page
-            response = requests.post(edit_url + "edit", data=data, auth=HTTPBasicAuth(username, password))
+            response = requests.post(edit_url, data=data, auth=HTTPBasicAuth(username, password))
             if response.status_code == 200:
                 return True
             else:
