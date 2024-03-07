b5f7762362a37d5771057e837b76d423a31703bb
nbiancolin <117390343+nbiancolin@users.noreply.github.com>
2024-03-07 13:06:45 -0500

    Revert "Merge branch 'main' of https://github.com/nbiancolin/wiki-updater"
    This reverts commit ac51cf5ec2c6b98ebe0b5e94ceb9427aadf2964a, reversing
    changes made to 29af7aac02ff7093a710e3e29ab8efc6e50fcff1.

--- parseGit.py
+++ parseGit.py
@@ -29,11 +29,10 @@ def closeSSH():
 def getGitLog(hours = globals.timeSince, testMode = False):
     print("Querying Git Log")
     if testMode:
-        client.exec_command(f'cd test ; git fetch')
+        client.exec_command(f'cd test ; git pull -r')
         stdin, stdout, stderr = client.exec_command(f'cd test ; git log --since="{hours} hours ago"')
     else:
-        #client.exec_command(f'cd ece297/work/mapper ; git pull -r')
-        client.exec_command(f'cd ece297/work/mapper ; git fetch')
+        client.exec_command(f'cd ece297/work/mapper ; git pull -r')
         stdin, stdout, stderr = client.exec_command(f'cd ece297/work/mapper ; git log --since="{hours} hours ago"')
     res = []
     for line in stdout:
